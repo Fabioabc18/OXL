76ec9cc7d3727d1c01174eda27d0e963d33dc3d8
Fábio cabral <fabio.brito.cabral@gmail.com>
2024-04-12 16:32:16 +0100

    vercel cfgs

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
 
-# Set up Python virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
-# Install requirements and collect static files
+
 pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput
