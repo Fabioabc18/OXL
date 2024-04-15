aa2afc2bcd0d3d38b68ef95525f892f2294050fe
Fábio cabral <fabio.brito.cabral@gmail.com>
2024-04-15 11:37:12 +0100

    alteração na disposição do preço e na filtragemd e anuncios

--- anuncios/models.py
+++ anuncios/models.py
@@ -6,7 +6,7 @@ from categorias.models import Categoria
 class Anuncio(models.Model):
     titulo = models.CharField(max_length=150, blank=False, null=False)
     subtitulo = models.CharField(max_length=255, blank=False, null=False)
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    preco = models.DecimalField(max_digits=10, decimal_places=3)
     data_post = models.DateTimeField(auto_now_add=True)
     descricao = models.TextField()
     id_utilizador = models.ForeignKey(User, on_delete=models.CASCADE)
